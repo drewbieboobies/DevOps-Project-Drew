e8248804cd7ca1ef38bb0378945dac332fde91a3
drewbieboobies <2302416I@student.tp.edu.sg>
2025-02-07 19:07:20 +0800

    Completed the implementation of real-time monitoring and logging

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ const { updateStudent, readAllStudents, getStudentById } = require('./utils/upda
 const studentsFilePath = 'C:\\Users\\offic\\Dvops project\\DevOps-Project\\utils\\students.json';
 
 var app = express();
+const logger = require('./logger');
 const PORT = process.env.PORT || 5050;
 var startPage = "course.html";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
+
+const statusMonitor = require('express-status-monitor');
+app.use(statusMonitor());
 
 const { editCourse, viewCourses} = require('./utils/updateCourseUtil');
 app.put('/edit-course/:id', editCourse);
@@ -40,6 +44,8 @@ server = app.listen(PORT, function () {
     const baseUrl = `http://${address.address == "::" ? 'localhost' :
         address.address}:${address.port}`;
     console.log(`Demo project at: ${baseUrl}`)
+    logger.info(`Demo project at: ${baseUrl}!`);
+    logger.error(`Example or error log`)
 });
 
 module.exports = { app, server }
